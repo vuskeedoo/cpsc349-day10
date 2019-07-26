740ce7f7baae15fa4fd2481132de8a56368e2ca8
Kevin <vu.kevin00@gmail.com>
2019-07-25 18:11:25 -0700

    first

---
+++ comments.js
@@ -0,0 +1,102 @@
+(function (window) {
+  $.ajax({
+    type:"GET",
+    url: "http://jsonplaceholder.typicode.com/posts",
+    success: function(data) {
+            console.log(data);
+            let body = document.getElementsByTagName("body");
+            body[0].innerHTML = "<h3>Posts and Comments</h3>";
+            let regex = /\n/g;
+            for(let i=0; i<data.length; i++) {
+              let article = document.createElement("article");
+              body[0].appendChild(article);
+              // title
+              let head = document.createElement("h2");
+              head.setAttribute("data-posts", "title");
+              head.innerText = data[i]["title"];
+              article.appendChild(head);
+              // paragraph
+              let para = document.createElement("P");
+              para.setAttribute("data-posts", "body");
+              para.innerHTML = data[i]["body"].replace(regex, '<br />');
+              article.appendChild(para);
+              // button
+              let button = document.createElement("button");
+              button.setAttribute("data-posts", "id");
+              button.setAttribute("value", data[i]["id"]);
+              button.innerText = "Show comments";
+              article.appendChild(button);
+              // section for comments
+              let section = document.createElement("section");
+              section.setAttribute("class", "comments");
+              section.setAttribute("id", "comments-"+data[i]["id"]);
+              section.hidden = true;
+              section.innerHTML = "<h3>Comments</h3>"
+              article.appendChild(section);
+            }
+        },
+    error: function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.status);
+        },
+   dataType: "jsonp"
+ });
+  'use strict';
+
+  const BUTTON_SELECTOR = '[data-posts="id"]';
+
+  let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+
+  buttons.forEach(function (button) {
+    'use strict';
+
+    let sectionSelector = `#comments-${button.value}`;
+    let commentSection = document.querySelector(sectionSelector);
+
+    button.addEventListener('click', function (event) {
+      if (commentSection.hidden) {
+        commentSection.hidden = false;
+        button.textContent = 'Hide comments';
+      } else {
+        commentSection.hidden = true;
+        button.textContent = 'Show comments';
+      }
+
+      event.preventDefault();
+    });
+  });
+})(window);
+
+function test() {
+}
+
+function generateComments(postid) {
+  let regex = /\n/g;
+  $.ajax({
+    type:"GET",
+    url: "http://jsonplaceholder.typicode.com/comments?postId="+postId,
+    success: function(data) {
+            //console.log(data);
+            let section = document.getElementsByTagName("section");
+            //section.innerHTML = "<h3>Comments</h3>";
+            console.log(section);
+            for(let i=0; i<data.length; i++) {
+              let para = document.createElement("P");
+              para.setAttribute("data-comments", "body");
+              para.innerHTML = data[i]["body"].replace(regex, '<br />');
+              section.appendChild(para);
+              let address = document.createElement("address");
+              address.setAttribute("data-comments", "name");
+              section.appendChild(address);
+              let a = document.createElement("a");
+              a.setAttribute("data-comments", "email");
+              a.setAttribute("href", "mailto:"+data[i]["email"]);
+              a.innerText = data[i]["name"];
+              address.appendChild(a);
+            }
+        },
+    error: function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.status);
+        },
+   dataType: "jsonp"
+  });
+}
