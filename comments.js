e15935853d56ca3a2ba1e7acd582de3cafabff9b
Kevin <vu.kevin00@gmail.com>
2019-07-26 06:57:00 -0700

    nvm this works, yay

--- comments.js
+++ comments.js
@@ -3,11 +3,11 @@
     type:"GET",
     url: "http://jsonplaceholder.typicode.com/posts",
     success: function(data) {
-            console.log(data);
+            //console.log(data);
             let body = document.getElementsByTagName("body");
             body[0].innerHTML = "<h3>Posts and Comments</h3>";
             let regex = /\n/g;
-            for(let i=0; i<data.length; i++) {
+            for(let i=0; i<data.length-1; i++) {
               let article = document.createElement("article");
               body[0].appendChild(article);
               // title
@@ -31,11 +31,43 @@
               section.setAttribute("class", "comments");
               section.setAttribute("id", "comments-"+data[i]["id"]);
               section.hidden = true;
-              section.innerHTML = "<h3>Comments</h3>"
+              section.innerHTML = "<h3>Comments</h3>";
               article.appendChild(section);
             }
-            for(let i=1; i<=data.length; i++) {
+            for(let i=0; i<=data.length-1; i++) {
               generateComments(data[i]["id"]);
+            }
+        },
+    error: function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.status);
+        },
+   dataType: "jsonp"
+ });
+})(window);
+
+function generateComments(postId) {
+  let regex = /\n/g;
+  $.ajax({
+    type:"GET",
+    url: "http://jsonplaceholder.typicode.com/comments?postId="+postId,
+    success: function(data) {
+            const COMMENT_SELECTOR = 'comments-'+postId;
+            let section = document.getElementById(COMMENT_SELECTOR);
+            //console.log(data);
+            for(let i=0; i<data.length-1; i++) {
+              let para = document.createElement("P");
+              para.setAttribute("data-comments", "body");
+              para.setAttribute("comment-id", data[i]["id"]);
+              para.innerHTML = data[i]["body"].replace(regex, '<br />');
+              section.appendChild(para);
+              let address = document.createElement("address");
+              address.setAttribute("data-comments", "name");
+              section.appendChild(address);
+              let a = document.createElement("a");
+              a.setAttribute("data-comments", "email");
+              a.setAttribute("href", "mailto:"+data[i]["email"]);
+              a.innerText = data[i]["name"];
+              address.appendChild(a);
             }
             // button toggle
             const BUTTON_SELECTOR = '[data-posts="id"]';
@@ -54,36 +86,6 @@
                 event.preventDefault();
               });
             });
-        },
-    error: function(jqXHR, textStatus, errorThrown) {
-            alert(jqXHR.status);
-        },
-   dataType: "jsonp"
- });
-})(window);
-
-function generateComments(postid) {
-  let regex = /\n/g;
-  $.ajax({
-    type:"GET",
-    url: "http://jsonplaceholder.typicode.com/comments?postId="+postid,
-    success: function(data) {
-            //console.log(data);
-            let section = document.getElementsByTagName("section");
-            for(let i=0; i<data.length; i++) {
-              let para = document.createElement("P");
-              para.setAttribute("data-comments", "body");
-              para.innerHTML = data[i]["body"].replace(regex, '<br />');
-              section[i].appendChild(para);
-              let address = document.createElement("address");
-              address.setAttribute("data-comments", "name");
-              section[i].appendChild(address);
-              let a = document.createElement("a");
-              a.setAttribute("data-comments", "email");
-              a.setAttribute("href", "mailto:"+data[i]["email"]);
-              a.innerText = data[i]["name"];
-              address.appendChild(a);
-            }
         },
     error: function(jqXHR, textStatus, errorThrown) {
             alert(jqXHR.status);
